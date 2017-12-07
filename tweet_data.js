63a59500f039d6e9406bd66779f235af169364b1
mohamedmansormemo <mohamedmansormemo67@gmail.com>
2017-12-07 11:36:28 +0200

    adding an express app && insert tweets into mongodb-NWY

--- tweet_data.js
+++ tweet_data.js
@@ -1,27 +1,30 @@
-// var Twitter = require('twitter-node-client').Twitter;
-
-// var error = function(err, response, body){
-//     console.log('ERROR [%s]', err);
-// };
-
-// var success = function(data){
-//     console.log('Data [%s]', data);
-// };
-// //Get this data from your twitter apps dashboard
-// var config = {
-//     "consumerKey": "VRfs5d3Mifdp3w3kDhlHDruMT",
-//     "consumerSecret": "xRmQflKlKniXh8zX0728Nz4xOfJGibzHTHE7Udjt3Z0jn4cPCP",
-//     "accessToken": "937716455554125825-LwiqsafHPDE3fIHH6z5OTSZUNdrVsgq",
-//     "accessTokenSecret": "OuXlBV00UxouNhoHXILqCU3NAVVDhBJ3icPbqiV3NXvmE",
-//     "callBackUrl": ""
-// };
-// var twitter = new Twitter(config);
-// var tweets = twitter.getHomeTimeline({count: '1'}, error, success); // return home fimeline.
+var Twitter = require('twitter-node-client').Twitter;
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/zappy";
 
+var error = function(err, response, body){
+    console.log('ERROR [%s]', err);
+};
+
+var success = function(data){
+    console.log('Data [%s]', data);
+};
+//Get this data from your twitter apps dashboard
+var config = {
+    "consumerKey": "VRfs5d3Mifdp3w3kDhlHDruMT",
+    "consumerSecret": "xRmQflKlKniXh8zX0728Nz4xOfJGibzHTHE7Udjt3Z0jn4cPCP",
+    "accessToken": "937716455554125825-LwiqsafHPDE3fIHH6z5OTSZUNdrVsgq",
+    "accessTokenSecret": "OuXlBV00UxouNhoHXILqCU3NAVVDhBJ3icPbqiV3NXvmE",
+    "callBackUrl": ""
+};
+var twitter = new Twitter(config);
+var tweets = twitter.getHomeTimeline({count: '1'}, error, success); // return home fimeline.
+
 MongoClient.connect(url, function(err, db){
     if (err) throw err;
-   console.log("DB Created");
-   db.close();
+    db.collection("zapppy").insertMany(tweets, function(err, res){
+        if (err) throw err;
+        console.log("Collection Created and tweets stored !");
+        db.close();
+    });
 });
